7ec2637b88dd283648f61168bb9229c507a27f22
cjshay <christopher.jackson.shay@gmail.com>
2019-02-17 23:42:40 -0500

    Comment out env line to test heroku deploy

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const logger = require('./logger');
 const bodyParser = require('body-parser');
-require('dotenv').config()
+// require('dotenv').config()
 const path = require('path')
 const fs = require('fs')
 const https = require('https')
